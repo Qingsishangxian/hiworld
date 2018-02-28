024e025a642abf0a808589b28c6152ebd8c513cb
Liyehe <liyehe@itheima.com>
2018-02-28 19:41:20 +0800

    test

---
+++ src/HelloWorld.java
@@ -0,0 +1,11 @@
+/**
+ * @author steven
+ * @version 1.0
+ * @description PACKAGE_NAME
+ * @date 2018/2/28
+ */
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println ("Hi World");
+    }
+}
